6d96ee79683e733b0ba0587ce4c431b1d8a012be
ainesmile <ainesmile@gmail.com>
2017-03-30 17:34:59 +1300

    add tags for article

--- blog/models.py
+++ blog/models.py
@@ -2,9 +2,25 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+class Tag(models.Model):
+    name = models.CharField(max_length=200)
+    created_date = models.DateTimeField('date created')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ('name', )
+
 class Article(models.Model):
     title = models.CharField(max_length=200)
-    text = models.CharField(max_length=300)
+    body = models.TextField("article body")
+    tags = models.ManyToManyField(Tag)
     pub_date = models.DateTimeField('date published')
+
+    class Meta:
+        ordering = ("pub_date",)
+
     def __str__(self):
         return self.title
+
